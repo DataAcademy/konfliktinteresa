b3ae8d7f8a5551c3f75de29b5b6f8ecd69d46aa7
Martin Keegan <martin@no.ucant.org>
2015-05-24 11:30:30 +0200

    add missing file

---
+++ bin/database.py
@@ -0,0 +1,8 @@
+import os
+from sqlite3 import dbapi2 as sqlite
+
+
+def get_db_handle():
+    path = os.path.join(os.path.dirname(__file__), "..", "db", "report_contents.db")
+    return sqlite.connect(path)
+
